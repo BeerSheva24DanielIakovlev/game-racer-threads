4501c7af6c240dd2502cc2935fb343b84cdd0df5
Daniel <gromdan90@gmail.com>
2024-11-24 23:01:52 +0200

    HW 43

--- src/main/java/telran/multithreading/Race.java
+++ src/main/java/telran/multithreading/Race.java
@@ -1,30 +1,46 @@
-
 package telran.multithreading;
 
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Race {
-	private int distance;
-	private int minSleep;
-	private int maxSleep;
-	AtomicInteger winner = new AtomicInteger(-1);
-	public Race(int distance, int minSleep, int maxSleep) {
-		this.distance = distance;
-		this.minSleep = minSleep;
-		this.maxSleep = maxSleep;
-	}
-	public int getWinner() {
-		return winner.get();
-	}
-	
-	public int getDistance() {
-		return distance;
-	}
-	public int getMinSleep() {
-		return minSleep;
-	}
-	public int getMaxSleep() {
-		return maxSleep;
-	}
-	
+    private int distance;
+    private int minSleep;
+    private int maxSleep;
+    private long startTime;
+    private final List<String> results = new ArrayList<>();
+    private final AtomicInteger placeCounter = new AtomicInteger(1);
+
+    public Race(int distance, int minSleep, int maxSleep) {
+        this.distance = distance;
+        this.minSleep = minSleep;
+        this.maxSleep = maxSleep;
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getMinSleep() {
+        return minSleep;
+    }
+
+    public int getMaxSleep() {
+        return maxSleep;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public synchronized void addResult(int racerNumber, long finishTime) {
+        long runningTime = finishTime - startTime;
+        results.add(String.format("%-6d %-12d %-15d",
+                placeCounter.getAndIncrement(), racerNumber, runningTime));
+    }
+
+    public List<String> getResults() {
+        return results;
+    }
 }
